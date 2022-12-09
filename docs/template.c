b7c04523fbd1af78b2eb69437c03f145597a8274
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-12-09 01:08:53 -0300

    File template

---
+++ docs/template.c
@@ -0,0 +1,26 @@
+/**
+ * @file archivo.c
+ * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
+ * @brief Descripcion
+ * @version 1.0
+ * @date YYYY-MM-DD
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+/* -------------------------------- Includes -------------------------------- */
+
+/* ---------------------- Private constants and macros ---------------------- */
+
+/* ------------------------------ Private enums ----------------------------- */
+
+/* ------------------ Private data structures and typedefs ------------------ */
+
+/* ---------------------------- Private typedefs ---------------------------- */
+
+/* -------------------- Private prototypes implementation ------------------- */
+
+/* -------------------- Public prototypes implementation -------------------- */
+// Getters
+// Setters
+// Utils
---
+++ docs/template.h
@@ -0,0 +1,35 @@
+#ifndef ARCHIVO_H
+#define ARCHIVO_H
+
+/**
+ * @file archivo.h
+ * @author Guido Rodriguez (guerodriguez@fi.uba.ar)
+ * @brief
+ * @version 0.1
+ * @date 2022-12-03
+ *
+ * @copyright Copyright (c) 2022. All rights reserved.
+ *
+ * Licensed under the MIT License, see LICENSE for details.
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+/* -------------------------------- Includes -------------------------------- */
+
+/* ------------------- Public data structures and typedefs ------------------ */
+
+/* ------------------------------- Constructor ------------------------------ */
+
+/* ------------------------------- Destructor ------------------------------- */
+
+/* ----------------------- Public constants and macros ---------------------- */
+
+/* ------------------------------ Public enums ------------------------------ */
+
+/* ---------------------------- Public prototypes --------------------------- */
+// Getters
+// Setters
+// Utils
+
+#endif    // ARCHIVO_H

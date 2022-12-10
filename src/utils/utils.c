c0ad7d9ef50c25beb92b8319b902e901f0225292
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-12-10 02:16:30 -0300

    Some useful functions to debug

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,20 +1,26 @@
-
-
 #include "utils.h"
-void dump_hex(const char *buf, size_t len) {
-    for (size_t i = 0; i < len; i++) {
-        printf("%02x ", (unsigned char)buf[i]);
-        if (i % 16 == 15) printf("\n");
+
+void dump_image_to_txt(const imagen_t *im, const char *filename) {
+    FILE *f = fopen(filename, "w");
+    if (f == NULL) return;
+
+    for (size_t y = 0; y < imagen_get_alto(im); y++) {
+        for (size_t x = 0; x < imagen_get_ancho(im); x++)
+            fprintf(f, "%d ", imagen_get_pixel(im, x, y));
+        fprintf(f, "\n");
     }
-    printf("\n");
 }
 
-void dump_ppm(const char *buf, size_t len, size_t ancho, size_t alto, const char *filename) {
+void dump_image_to_ppm(const imagen_t *im, const char *filename) {
     FILE *f = fopen(filename, "w");
-    fprintf(f, "P3 %zu %zu 255\n", ancho, alto);
-    for (size_t i = 0; i < len; i++) {
-        fprintf(f, "%d ", (unsigned char)buf[i]);
-        if (i % 3 == 2) fprintf(f, "\n");
+    if (f == NULL) return;
+
+    fprintf(f, "P3 %zu %zu 255\n", imagen_get_ancho(im), imagen_get_alto(im));
+    for (size_t y = 0; y < imagen_get_alto(im); y++) {
+        for (size_t x = 0; x < imagen_get_ancho(im); x++) {
+            pixel_t pixel = imagen_get_pixel(im, x, y);
+            fprintf(f, "%d %d %d ", pixel, pixel, pixel);
+        }
+        fprintf(f, "\n");
     }
-    fclose(f);
 }
